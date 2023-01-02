2269a70fbf3be76955f65edc974a583f97b62b1b
Vishal Vijayakumar <vishal@expectlabs.com>
2023-01-01 23:23:36 -0800

    login screen fetch token

--- web/src/pages/Login.jsx
+++ web/src/pages/Login.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { useState } from 'react';
 
-function Login() {
-  return (
+
+export const Login = () => {
+    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        const opts = {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'email': email,
+                'password': password
+            })
+        }
+        fetch('http://192.168.0.74:3001/api/token', opts)
+            .then(resp => {
+                if(resp.status === 200) return resp.json();
+                else alert("There was an issue");
+            })
+            .then(data =>{
+                sessionStorage.setItem('token', data.access_token);
+                console.log(data.access_token);
+            })
+            .catch(error =>{
+                console.error(error);
+            })
+
+    }
+
+    return (
     <div className='formContainer'>
         <div className='formWrapper'>
             <span className='logo'>React App</span>
             <span className='title'>Login</span>
-            <form>
-                <input type="email" placeholder='email'/>
-                <input type="password" placeholder='password'/>
-                <button>Sign In</button>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder='email' name='email' onChange={event => setEmail(event.target.value)}/>
+                <input type="password" placeholder='password' name='password' onChange={event => setPassword(event.target.value)}/>
+                <button type='submit'>Sign In</button>
             </form>
             <p>Don't have an account? <a href="/Register">Register</a></p>
         </div>
@@ -17,4 +52,4 @@ function Login() {
   )
 }
 
-export default Login
+export default Login;
